327c75d27def8fd8d082053713964d6449e508d7
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-05-19 13:56:59 +0500

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import authReducer from './authSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'], // only persist the auth reducer
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+export const store = configureStore({
+  reducer: {
+    auth: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
